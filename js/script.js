0ddeeccb091694c859de2d7d24e4f0409f468fdb
Felipe Almeida <felipe.lipe12.fl@outlook.com>
2024-10-03 12:14:04 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,129 @@
+let playerScore = 0;
+let computerScore = 0;
+const maxScore = 3;
+
+function computerChoices() {
+    const allChoices = ['rock', 'paper', 'scissor'];
+    const randomChoice = Math.floor(Math.random() * 3);
+    return allChoices[randomChoice];
+}
+
+function mappingChoices(playerChoice, computerChoice) {
+    const iconsMap = {
+        rock: 'fa-hand-fist',
+        paper: 'fa-hand',
+        scissor: 'fa-hand-peace',
+        unknown: 'fa-question'
+    };
+
+    const namesMap = {
+        rock: 'Rock',
+        paper: 'Paper',
+        scissor: 'Scissor',
+        unknown: '???'
+    };
+
+    const playerIcon = document.getElementById('player-icon');
+    const playerName = document.getElementById('player-name');
+    playerIcon.className = `fa-solid ${iconsMap[playerChoice]}`;
+    playerName.textContent = namesMap[playerChoice];
+
+    const computerIcon = document.getElementById('computer-icon');
+    const computerName = document.getElementById('computer-name');
+    computerIcon.className = `fa-solid ${iconsMap[computerChoice]}`;
+    computerName.textContent = namesMap[computerChoice];
+}
+
+function possibilitiesResults(playerChoice, computerChoice) {
+    const rulesGame = {
+        rock: 'scissor',
+        paper: 'rock',
+        scissor: 'paper'
+    };
+
+    if (playerChoice === computerChoice) {
+        return 'Draw!!!';
+    }
+
+    if (rulesGame[playerChoice] === computerChoice) {
+        playerScore++;
+        return 'You win!!!';
+    } else {
+        computerScore++;
+        return 'You lose!!!';
+    }
+}
+
+function updateScore() {
+    document.getElementById('player-score').textContent = playerScore;
+    document.getElementById('computer-score').textContent = computerScore;
+}
+
+function showPopup(message, isFinish) {
+    const popup = document.getElementById('popup-gameover');
+    const popupMessage = document.getElementById('popup-message');
+    const popupButton = document.getElementById('popup-button');
+
+    popupMessage.textContent = message;
+    popupButton.textContent = isFinish ? 'Restart' : 'Continue';
+    popup.classList.remove('hidden');
+    popup.classList.add('show');
+}
+
+function hidePopup() {
+    const popup = document.getElementById('popup-gameover');
+    popup.classList.add('hidden');
+    popup.classList.remove('show');
+}
+
+function finishGame() {
+    if (playerScore === maxScore || computerScore === maxScore) {
+        const message = playerScore === maxScore ? 'Você venceu o jogo!' : 'O computador venceu o jogo!';
+        showPopup(message, true);
+        return true;
+    }
+    return false;
+}
+
+function restartGame() {
+    playerScore = 0;
+    computerScore = 0;
+    updateScore();
+
+    mappingChoices('unknown', 'unknown');
+}
+
+function playerChoices(playerChoice) {
+    const computerChoice = computerChoices();
+
+    mappingChoices(playerChoice, computerChoice);
+
+    setTimeout(() => {
+        const winner = possibilitiesResults(playerChoice, computerChoice);
+
+        updateScore();
+
+        const isFinish = finishGame();
+
+        if (!isFinish) {
+            showPopup(winner, false);
+        }
+    }, 500);
+}
+
+document.querySelectorAll('.jankenpon__buttons--button').forEach(button => {
+    button.addEventListener('click', () => {
+        const playerChoice = button.getAttribute('data-choice');
+        playerChoices(playerChoice);
+    });
+});
+
+document.getElementById('popup-button').addEventListener('click', () => {
+    const popupButton = document.getElementById('popup-button');
+
+    if (popupButton.textContent === 'Restart') {
+        restartGame();
+    }
+
+    hidePopup();
+});
